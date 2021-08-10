6761a23404e8aa6fcdbf2625a89559896b5db153
Jaymari Timblique <timbliquejaymari@gmail.com>
2021-08-10 10:45:41 +0800

    Commit: Updated files for Section 1

---
+++ 01-starting-project/app.js
@@ -0,0 +1,32 @@
+// The Javascript way
+/*
+const buttonEl = document.querySelector("button");
+const inputEl = document.querySelector("input");
+const listEl = document.querySelector("ul");
+
+function addGoal() {
+  const enteredValue = inputEl.value;
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = enteredValue;
+  listEl.appendChild(listItemEl);
+  inputEl.value = "";
+}
+
+buttonEl.addEventListener("click", addGoal);
+*/
+
+// The Vue way
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
