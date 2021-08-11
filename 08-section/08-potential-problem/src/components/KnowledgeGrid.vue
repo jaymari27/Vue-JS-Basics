17b400128e2b204c3752a683452eb8bc6dadc0ae
Jaymari Timblique <timbliquejaymari@gmail.com>
2021-08-11 13:07:23 +0800

    Added files for next lesson, updated folder structure

---
+++ 08-section/08-potential-problem/src/components/KnowledgeGrid.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  // references the data we declared under provide. NOTE: You can only inject data from a sub-/parent component. Since the provide object is under App.vue, it is a parent of KnowledgeGrid, which allows us to reference its data.
+  inject: ['topics']
+};
+</script>
