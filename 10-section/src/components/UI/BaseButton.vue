2b01efe1da643cc97179fcf5835d79b26a5d9b19
Jaymari Timblique <timbliquejaymari@gmail.com>
2021-08-12 10:23:24 +0800

    Commit: Finished section10

---
+++ 10-section/src/components/UI/BaseButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api';
+
+export default {
+  props: ['mode']
+  // data() {}
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #9d60c5;
+  border: 1px solid #3a0061;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #7a00cc;
+  border-color: #270041;
+}
+
+.flat {
+  background-color: transparent;
+  color: #3c0064;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #edd2ff;
+}
+</style>
