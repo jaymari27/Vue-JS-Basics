f30b48a99bc8cb5208290d6d51e4003f7319dca2
Jaymari Timblique <timbliquejaymari@gmail.com>
2021-08-16 12:26:34 +0800

    Updated login functionality and request sending

--- 16-coach-finder/src/store/modules/requests/actions.js
+++ 16-coach-finder/src/store/modules/requests/actions.js
@@ -28,8 +28,10 @@ export default {
   },
   async fetchRequests(context) {
     const coachId = context.rootGetters.userId;
+    const token = context.rootGetters.token;
     const response = await fetch(
-      `https://vue-http-demo-80ef9-default-rtdb.asia-southeast1.firebasedatabase.app/${coachId}.json`
+      `https://vue-http-demo-80ef9-default-rtdb.asia-southeast1.firebasedatabase.app/${coachId}.json?auth=` +
+        token
     );
     const responseData = await response.json();
 
